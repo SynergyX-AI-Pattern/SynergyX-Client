4958b67f50c5dd0ab846d9e12c099d0c428d73e8
KaHeyon <diddl2002@naver.com>
2025-10-10 20:05:19 +0900

    [REFACTOR] 관심 종목 페이지 UI 배치 및 간격 조정 #39

--- lib/widgets/interest/BacktestResultCard.dart
+++ lib/widgets/interest/BacktestResultCard.dart
@@ -7,6 +7,7 @@ import 'dart:math' as math;
 
 class BacktestResultCard extends StatelessWidget {
   final BacktestResult result;
+
   const BacktestResultCard({super.key, required this.result});
 
   double _truncate(num v, int decimals) {
@@ -15,44 +16,52 @@ class BacktestResultCard extends StatelessWidget {
   }
 
   String _fmtPct(
-      num v, {
-        bool inputIsRatio = false, // true면 0.053 -> 5.3%
-        int decimals = 2,
-        bool round = true,
-      }) {
+    num v, {
+    bool inputIsRatio = false, // true면 0.053 -> 5.3%
+    int decimals = 2,
+    bool round = true,
+  }) {
     final p = (inputIsRatio ? v * 100 : v.toDouble());
-    final d = round ? double.parse(p.toStringAsFixed(decimals))
-        : _truncate(p, decimals);
+    final d =
+        round
+            ? double.parse(p.toStringAsFixed(decimals))
+            : _truncate(p, decimals);
     return '${d.toStringAsFixed(decimals)}%';
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 8),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-
-          // 실행일, 매칭 횟수
-          Row(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+          child: Column(
             children: [
-              Text('실행 날짜 : ', style: TextStyles.partName),
-              Text('${result.executedAt}', style: TextStyles.valueText),
-              const SizedBox(width: 10),
-              Text('매칭 횟수 : ', style: TextStyles.partName),
-              Text('${result.matchedCount}', style: TextStyles.valueText),
+              // 실행일, 매칭 횟수
+              Row(
+                children: [
+                  Text('실행 날짜 : ', style: TextStyles.partName),
+                  Text('${result.executedAt}', style: TextStyles.valueText),
+                  const SizedBox(width: 10),
+                  Text('매칭 횟수 : ', style: TextStyles.partName),
+                  Text('${result.matchedCount}', style: TextStyles.valueText),
+                ],
+              ),
+              const SizedBox(height: 12),
+
+              // 백테스팅 캔들차트
+              BacktestCandleChart(backtestId: result.backtestId),
             ],
           ),
-          const SizedBox(height: 12),
+        ),
 
-          // 백테스팅 캔들차트
-          BacktestCandleChart(backtestId: result.backtestId),
+        const SizedBox(height: 16),
 
-          const SizedBox(height: 16),
-
-          // 기간 + 더보기 버튼
-          Row(
+        // 기간 + 더보기 버튼
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Row(
             children: [
               Text('기간 : ', style: TextStyles.partName),
               Text(
@@ -62,12 +71,13 @@ class BacktestResultCard extends StatelessWidget {
               const Spacer(),
               TextButton(
                 onPressed: () {
-                  // ✅ 백테스트 상세 페이지로 이동
+                  // 백테스트 상세 페이지로 이동
                   Navigator.of(context).push(
                     MaterialPageRoute(
-                      builder: (_) => BacktestResultScreen(
-                        result: result.toJson(), // ← Map으로 변환해서 전달
-                      ),
+                      builder:
+                          (_) => BacktestResultScreen(
+                            result: result.toJson(), // ← Map으로 변환해서 전달
+                          ),
                     ),
                   );
                 },
@@ -83,28 +93,28 @@ class BacktestResultCard extends StatelessWidget {
               ),
             ],
           ),
-
-          const SizedBox(height: 8),
-
-          // 지표
-          InfoCardGroup(
+        ),
+        // 지표
+        Transform.translate(
+          offset: const Offset(0, -6),
+          child: InfoCardGroup(
             rows: [
               {'label': '승률', 'value': _fmtPct(result.winRate, decimals: 1)},
               {
                 'label': '평균 수익',
                 'value': _fmtPct(result.averageReturn, decimals: 2),
-                'color': const Color(0xFF1573FE)
+                'color': const Color(0xFF1573FE),
               },
               {
                 'label': '최대 수익',
                 'value': _fmtPct(result.maxReturn, decimals: 2, round: false),
                 'subValue': result.maxReturnDate,
-                'color': const Color(0xFF1573FE)
+                'color': const Color(0xFF1573FE),
               },
             ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
